2f097261996f70c9e8cf517bc1876a486f2f086f
Avinash Panchal <avinash0702panchal@gmail.com>
2024-08-17 14:33:46 +0530

    js weired behaviour

--- dataTypes.js
+++ dataTypes.js
@@ -1,9 +1,9 @@
 "use strict"; // treat all JS code as newer version
 
 /*
-    number 
+    Number 
     bigint
-    string
+    String
     boolean
     null
     undefined
@@ -13,5 +13,39 @@
 
 */
 
-console.log(typeof undefined);
-console.log(typeof null)
+// console.log(typeof undefined);
+// console.log(typeof null)
+
+
+let num = "33a";
+
+let toNum = Number(num);
+
+
+console.log(toNum)
+console.log(typeof NaN)
+
+console.log(1 + "2")
+console.log("1" + 1 + 2);
+console.log(1 + 1 + "2");
+
+// this happens due to ecmascript guidelines
+
+
+let firsName = 'avinash';
+let lastName = ' Panchal';
+
+// console.log(firsName + lastName );
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2")
+
+
+// console.log(+true);
+// console.log(+"")
+
+
+let counter = 100;
+console.log(counter++)
