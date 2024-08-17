e038bc7acc4aad36ef02280db17208345c65d2b0
Avinash Panchal <avinash0702panchal@gmail.com>
2024-08-17 13:53:07 +0530

    dataTypes in js

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+/*
+    number 
+    bigint
+    string
+    boolean
+    null
+    undefined
+    symbol
+
+    object 
+
+*/
+
+console.log(typeof undefined);
+console.log(typeof null)
